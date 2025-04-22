0d6a1b02ded810720acf0216e302297b9dec624a
beepboop32 <157918068+beepboop32@users.noreply.github.com>
2025-04-22 14:54:56 +0000

    changed contact page and fixed usues to quiz

---
+++ Contact/contactJS.js
@@ -0,0 +1,31 @@
+
+
+
+
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const feedback = document.getElementById("feedback");
+    feedback.style.display = "block";
+
+    const form = document.getElementById("contactForm");
+    const fields = form.querySelectorAll("input[type='text'], textarea, select");
+    let allFilled = true;
+    //remove whitespace from inputs bc people are silly billy (me testing)
+    fields.forEach(field => {
+        if (!field.value.trim()) {
+            allFilled = false;
+        }
+    });
+
+    //validation check for all the fields
+    
+    if (allFilled) {
+        feedback.textContent = "Form submitted successfully!";
+        feedback.style.color = "green";
+        feedback.style.fontWeight = "bold";
+    } else {
+        feedback.textContent = "Please fill out all fields.";
+        feedback.style.color = "red";
+        feedback.style.fontWeight = "bold";
+    }
+});
